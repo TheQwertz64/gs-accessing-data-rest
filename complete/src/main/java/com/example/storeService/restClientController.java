d5d0b72825d61f38bebed5161aa3a35c913322b3
TheQwertz64 <47073492+TheQwertz64@users.noreply.github.com>
2024-04-22 16:56:39 -0400

    nil

--- complete/src/main/java/com/example/storeService/restClientController.java
+++ complete/src/main/java/com/example/storeService/restClientController.java
@@ -2,13 +2,9 @@ package com.example.storeService;
 
 import java.util.List;
 import java.util.ArrayList;
-import org.springframework.web.bind.annotation.RestController;
-import org.slf4j.LoggerFactory;
-import org.slf4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
@@ -28,7 +24,6 @@ public class restClientController implements restClientInterface {
         service.saveTv(myTv);
 
     }
-
 
     @PostMapping("/silly")
     public String silly(@RequestBody String info) {
