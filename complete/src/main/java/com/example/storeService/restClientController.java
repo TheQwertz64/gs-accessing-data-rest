dac3acac0d6f41261585af591515bddbaecb281b
TheQwertz64 <47073492+TheQwertz64@users.noreply.github.com>
2024-04-23 16:35:43 -0400

    marked as controller

--- complete/src/main/java/com/example/storeService/restClientController.java
+++ complete/src/main/java/com/example/storeService/restClientController.java
@@ -5,9 +5,11 @@ import java.util.ArrayList;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
+@RestController
 public class restClientController implements restClientInterface {
 
     @Autowired
